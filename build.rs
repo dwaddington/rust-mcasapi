bf059aa05b76a1bcb9b98cced3f5f32d0c66b995
Daniel Waddington <daniel.waddington@ibm.com>
2022-01-24 17:47:19 -0800

    repo init

---
+++ build.rs
@@ -0,0 +1,17 @@
+use std::env;
+
+fn main()
+{
+    let mcas_build_dir = env::var("MCAS_LIB_DIR").unwrap(); // path to MCAS libraries
+    println!("cargo:rustc-link-search=native=/usr/lib64/");
+    println!("cargo:rustc-link-search=native=/usr/lib/");
+    let search_native = "cargo:rustc-link-search=native=".to_owned() + &mcas_build_dir;
+    println!("{}",search_native);
+    println!("cargo:rustc-link-lib=common");
+    println!("cargo:rustc-link-lib=pthread");
+    println!("cargo:rustc-link-lib=numa");
+    println!("cargo:rustc-link-lib=dl");
+    println!("cargo:rustc-link-lib=rt");
+    println!("cargo:rustc-link-lib=mcasapi");
+}
+
